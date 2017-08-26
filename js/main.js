a951d37be26efcf30243b43429e49f0b9425d69a
shashi <sasitech18@gmail.com>
2017-08-26 09:52:32 +0100

    Added js code to main.js file

--- js/main.js
+++ js/main.js
@@ -24,10 +24,10 @@ var tipsList = [
 ];
 
 // Tip Limit counter
-
+var tipLimit = 3;
 
 // Generate a number
-function generateNumber(){
+function generateNumber() {
 	return Math.floor(Math.random() * tipsList.length);
 }
 
@@ -35,7 +35,13 @@ function generateNumber(){
 // 1. Get random number from generateNumber()
 // 2. Use the random number to get the tip from the array
 // 3. Show the tip
-
+function generateTip() {
+	var tip = tipsList[generateNumber()];
+	var tipElement = document.querySelector('.js-tip');
+  var tipLimitCount = document.querySelector('.tip-limit-count');
+  tipElement.innerHTML = tip;
+	tipLimitCount.innerHTML = tipLimit;
+}
 
 // Tip button click
 // 1. Select the tip button
@@ -45,5 +51,21 @@ function generateNumber(){
 // 3b. If the tipLimit is still above or equal to 0, generate a new tip
 // 3c. If not, change the button text and look
 
+function onTipButtonClick() {
+	var tipButton = document.querySelector('.tip-button');
+	tipButton.addEventListener('click',
+	 function() {
+		 tipLimit = tipLimit - 1;
+		 if (tipLimit >= 0) {
+		   generateTip();
 
+			if (tipLimit === 0) {
+				tipButton.innerHTML = 'See you in anothet tab!';
+				tipButton.classList.add('disabled');
+			}
+	 }
+ });
+}
+   onTipButtonClick();
 // Get the first tip
+generateTip(); // Get the first tip
